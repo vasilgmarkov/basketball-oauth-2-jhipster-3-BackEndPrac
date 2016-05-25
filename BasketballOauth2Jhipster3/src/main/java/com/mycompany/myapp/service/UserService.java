cf3e88b6fc74d4bf753d17665b4e13b9bee201e2
vasil <vasilgmarkov@gmail.com>
2016-05-25 12:04:34 -0700

    La activacion de usuario es automatica esta en "true" y logea automaticamente despues de registrarse.

--- BasketballOauth2Jhipster3/src/main/java/com/mycompany/myapp/service/UserService.java
+++ BasketballOauth2Jhipster3/src/main/java/com/mycompany/myapp/service/UserService.java
@@ -94,8 +94,8 @@ public class UserService {
         newUser.setLastName(lastName);
         newUser.setEmail(email);
         newUser.setLangKey(langKey);
-        // new user is not active
-        newUser.setActivated(false);
+        // new user is active
+        newUser.setActivated(true);
         // new user gets registration key
         newUser.setActivationKey(RandomUtil.generateActivationKey());
         authorities.add(authority);
